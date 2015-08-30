070a3cf0aaf79ca11b46b3b653b3b2394b436a89
Ozan Deniz <denzozan@gmail.com>
2015-08-30 16:50:40 +0300

    Initial Commit

---
+++ GETServer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GETServer
+//
+//  Created by Ozan Deniz on 30/08/15.
+//  Copyright (c) 2015 Ozan Deniz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
